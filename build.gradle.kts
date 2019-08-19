ab369bcf7203600b89d36297412b5d4f86761aae
Simon Ogorodnik <Simon.Ogorodnik@jetbrains.com>
2019-08-19 15:39:34 +0300

    Local publication

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,11 +2,13 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.3.40-eap-105"
+    `maven-publish`
 }
 
 subprojects {
     apply(plugin = "kotlin")
-    group = "org.jetbrains"
+    apply(plugin = "maven-publish")
+    group = "org.jetbrains.muTrace"
     version = "1.0-SNAPSHOT"
 
     repositories {
@@ -21,5 +23,13 @@ subprojects {
 
     tasks.withType<KotlinCompile> {
         kotlinOptions.jvmTarget = "1.8"
+    }
+
+    publishing {
+        publications {
+            create<MavenPublication>("maven") {
+                from(components["java"])
+            }
+        }
     }
 }
